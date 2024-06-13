34c8a94f4869257299e920b576b629e51852fae8
Kristopher Morales Zamudio <krismoralesz@gmail.com>
2024-06-13 11:21:50 -0700

    Create Message Service

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,43 @@
 package org.example;
 
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        int option = 0;
+
+        do {
+            System.out.println("-----------------");
+            System.out.println("Messages Application");
+            System.out.println("1. Create a new message");
+            System.out.println("2. Display all messages");
+            System.out.println("3. Edit message");
+            System.out.println("4. Delete message");
+            System.out.println("5. Exit");
+
+            option = scanner.nextInt();
+
+            switch (option) {
+                case 1:
+                    MessagesService.createMessage();
+                    break;
+                case 2:
+                    MessagesService.showMessage();
+                    break;
+                case 3:
+                    MessagesService.updateMessage();
+                    break;
+                case 4:
+                    MessagesService.deleteMessage();
+                    break;
+                default:
+                    break;
+            }
         }
+        while (option != 5);
     }
 }
---
+++ src/main/java/org/example/MessagesService.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class MessagesService {
+
+    public  static void createMessage() {}
+
+    public static void showMessage() {}
+
+    public  static void updateMessage() {}
+
+    public  static void deleteMessage() {}
+}
